9b5041d70151517c01097e092444d75f734eca59
Konstantin Sargsyan <konstantin@manot.ai>
2023-08-08 12:19:38 +0400

    fix progress

--- src/manot/manot.py
+++ src/manot/manot.py
@@ -318,9 +318,11 @@ class manotAI:
 
     def __check_progress(self, process_method, id):
         progress = 0
+        status = 'started'
         progress_bar = tqdm(desc="Progress", total=100)
-        while progress < 100:
+        while status == 'started':
             result = process_method(id)
+            status = result['status']
             if result:
                 if result["status"] != "failure":
                     progress_bar.update(int(result['progress'] - progress))
